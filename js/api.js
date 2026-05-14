5673f0357e6cf2442b0368186a7f2ff3f27b2375
Lincoln Garcez <lincoln.garcez@sefaz.am.gov.br>
2026-05-14 17:07:29 -0400

    Commit

--- js/api.js
+++ js/api.js
@@ -7,6 +7,20 @@ const api = {
             alert('Erro ao busca pensamentos')
             throw error
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+        } catch (error) {
+
+        }
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
+
+    const formmularioPensamento = document.getElementById("pensamento-form")
+    formmularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+
+    const botaoCancelar = document.getElementById("botao-cancelar")
+    botaoCancelar.addEventListener("click", ui.cancelarPensamento)
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        await api.salvarPensamento({conteudo, autoria})
+        ui.renderizarPensamentos()
+    } catch(error) {
+        alert("Erro ao salvar pensamento")
+    }
+}
